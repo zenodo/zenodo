b2100f8718e0fcc40feb19f9173dbf35185d25bb
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2013-02-27 21:34:34 +0100

    WebStyle: fix breadcrumb for legacy pages
    * Adds correct style to the navtrail for legacy pages
    
    * Breadcrumb rendering based on Jinja2

--- webstyle/lib/webstyle_templates_openaire.py
+++ webstyle/lib/webstyle_templates_openaire.py
@@ -22,58 +22,49 @@ WebStyle templates. Customize the look of pages of Invenio
 import cgi
 
 from invenio.config import \
-     CFG_SITE_LANG, \
-     CFG_SITE_NAME, \
-     CFG_SITE_NAME_INTL, \
-     CFG_SITE_SUPPORT_EMAIL, \
-     CFG_SITE_SECURE_URL, \
-     CFG_SITE_URL, \
-     CFG_VERSION, \
-     CFG_WEBSTYLE_INSPECT_TEMPLATES, \
-     CFG_WEBSTYLE_TEMPLATE_SKIN
+    CFG_SITE_LANG, \
+    CFG_SITE_NAME, \
+    CFG_SITE_NAME_INTL, \
+    CFG_SITE_SUPPORT_EMAIL, \
+    CFG_SITE_SECURE_URL, \
+    CFG_SITE_URL, \
+    CFG_VERSION, \
+    CFG_WEBSTYLE_INSPECT_TEMPLATES, \
+    CFG_WEBSTYLE_TEMPLATE_SKIN
 from invenio.messages import gettext_set_language, is_language_rtl
 from invenio.dateutils import convert_datecvs_to_datestruct, \
-                              convert_datestruct_to_dategui
+    convert_datestruct_to_dategui
 from invenio.webstyle_templates import Template as InvenioTemplate
 from invenio.urlutils import create_html_link
+from invenio.webinterface_handler_flask_utils import _
 
 from flask import render_template
+from bs4 import BeautifulSoup
 
 
 class Template(InvenioTemplate):
+
+    def tmpl_navtrailbox_body(self, ln, title, previous_links, separator,
+                              prolog, epilog):
+        """ Bootstrap friendly-Create navigation trail box body """
+        if title == CFG_SITE_NAME_INTL.get(ln, CFG_SITE_NAME):
+            return ""
+
+        # First element
+        breadcrumbs = [(_("Home"), CFG_SITE_URL), ]
+
+        # Decode previous elements
+        if previous_links:
+            soup = BeautifulSoup(previous_links)
+            for link in soup.find_all('a'):
+                breadcrumbs.append((unicode(" ".join(link.contents)), link.get('href')))
+
+        # Add head
+        if title:
+            breadcrumbs.append((title, ''))
+
+        return render_template("legacy_navtrailbox_body.html",
+                               breadcrumbs=breadcrumbs).encode('utf8')
+
     def tmpl_page(self, req, **kwargs):
-        """
-          - req
-          # description
-          # keywords
-          userinfobox
-          useractivities_menu
-          adminactivities_menu
-          navtrailbox
-          pageheaderadd
-          # boxlefttop
-          # boxlefttopadd
-          # boxleftbottom
-          # boxleftbottomadd
-          # boxrighttop
-          # boxrighttopadd
-          # boxrightbottom
-          # boxrightbottomadd
-          # titleprologue
-          # title
-          # titleepilogue
-          # body
-          lastupdated
-          # pagefooteradd
-          uid
-          secure_page_p
-          navmenuid
-          metaheaderadd
-          rssurl
-          show_title_p
-          body_css_classes
-          show_header
-          show_footer
-        """
-     
         return render_template("legacy_page.html", **kwargs).encode('utf8')
