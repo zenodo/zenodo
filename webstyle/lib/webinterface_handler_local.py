5e4b621c19b7cbc29228773f517dd0f983097b25
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2013-04-24 09:55:03 +0200

    WebStyle: add temporary cache tag
    * The Flask-Cache Jinja2 cache-tag is using cache keys based on
      filename and lineno. Two commits fixing the issue has been submitted
      for integration, but until integrated, this commit adds a cache
      tag uses stable, predictable keys.

--- webstyle/lib/webinterface_handler_local.py
+++ webstyle/lib/webinterface_handler_local.py
@@ -21,6 +21,12 @@ OpenAIRE local customization of Flask application
 """
 
 from invenio.textutils import nice_size
+from invenio.signalutils import webcoll_after_webpage_cache_update
+from jinja2 import nodes
+from jinja2.ext import Extension
+
+JINJA_CACHE_ATTR_NAME = '_template_fragment_cache'
+TEMPLATE_FRAGMENT_KEY_TEMPLATE = '_template_fragment_cache_%s%s'
 
 
 def customize_app(app):
@@ -60,5 +66,80 @@ def customize_app(app):
     # Removed unwanted invenio menu items
     #
     del app.config['menubuilder_map']['main'].children['help']
-    del app.config['menubuilder_map']['main'].children['personalize']
-    #del app.config['menubuilder_map']['main'].children['webdeposit']
+
+    # Add {% zenodocache tag %} and connect webcoll signal handler to invalidate
+    # cache.
+    app.jinja_env.add_extension(ZenodoExtension)
+    webcoll_after_webpage_cache_update.connect(invalidate_jinja2_cache, app)
+
+
+def make_template_fragment_key(fragment_name, vary_on=[]):
+    """
+    Make a cache key for a specific fragment name
+    """
+    if vary_on:
+        fragment_name = "%s_" % fragment_name
+    return TEMPLATE_FRAGMENT_KEY_TEMPLATE % (fragment_name, "_".join(vary_on))
+
+
+def invalidate_jinja2_cache(sender, collection=None, lang=None):
+    """
+    Invalidate collection cache
+    """
+    from invenio.cache import cache
+    cache.delete(make_template_fragment_key(collection.name, vary_on=[lang]))
+
+
+class ZenodoExtension(Extension):
+    """
+    Temporary extension (let's see how long it will stay ;-).
+
+    This extension is made until a pull-request has been integrated in the
+    main Flask-Cache branch, so that generated cache keys are stable and
+    predictable instead of based on filename and line numbers.
+    """
+
+    tags = set(['zenodocache'])
+
+    def parse(self, parser):
+        lineno = parser.stream.next().lineno
+
+        # Parse timeout
+        args = [parser.parse_expression()]
+
+        # Parse fragment name
+        parser.stream.skip_if('comma')
+        args.append(parser.parse_expression())
+
+        # Parse vary_on parameters
+        vary_on = []
+        while parser.stream.skip_if('comma'):
+            vary_on.append(parser.parse_expression())
+
+        if vary_on:
+            args.append(nodes.List(vary_on))
+        else:
+            args.append(nodes.Const([]))
+
+        body = parser.parse_statements(['name:endcache'], drop_needle=True)
+        return nodes.CallBlock(self.call_method('_cache', args),
+                               [], [], body).set_lineno(lineno)
+
+    def _cache(self, timeout, fragment_name, vary_on,  caller):
+        try:
+            cache = getattr(self.environment, JINJA_CACHE_ATTR_NAME)
+        except AttributeError, e:
+            raise e
+
+        key = make_template_fragment_key(fragment_name, vary_on=vary_on)
+
+        # Delete key if timeout is 'del'
+        if timeout == "del":
+            cache.delete(key)
+            return caller()
+
+        rv = cache.get(key)
+        if rv is None:
+            rv = caller()
+            cache.set(key, rv, timeout)
+        return rv
