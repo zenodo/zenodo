f0e027024577cbe4a8a1941e05419083ee8e6270
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-03-12 16:26:19 +0100

    inspire: initial skeleton
    * Adds initial skeleton for an INSPIRE module that will allow
      adding a link to a record.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

---
+++ zenodo/modules/inspire/views.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of ZENODO.
+# Copyright (C) 2014 CERN.
+#
+# ZENODO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ZENODO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ZENODO. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+
+"""
+INSPIRE webhook endpoint
+"""
+
+from __future__ import absolute_import
+
+from flask import Blueprint, current_app
+
+from invenio.modules.webhooks.models import Receiver, CeleryReceiver
+from ..tasks import handle_inspire_payload
+
+
+blueprint = Blueprint(
+    'zenodo_inspire',
+    __name__,
+)
+
+
+#
+# Before first request loaders
+#
+@blueprint.before_app_first_request
+def register_webhook():
+    """
+    Setup webhook endpoint for github notifications
+    """
+    Receiver.register(
+        current_app.config.get('INSPIRE_WEBHOOK_RECEIVER_ID'),
+        CeleryReceiver(handle_inspire_payload)
+    )
