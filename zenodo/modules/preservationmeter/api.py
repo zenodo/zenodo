0f22151a8b1a3c37bd22e5aa1c26052a937e5f78
João Gonçalves <jsvgoncalves@gmail.com>
2014-08-14 23:17:35 +0200

    preservationmeter: Refactor and testing
    * Refactored lots of code on API
    * Added a test
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- zenodo/modules/preservationmeter/api.py
+++ zenodo/modules/preservationmeter/api.py
@@ -20,22 +20,7 @@
 ## granted to it by virtue of its status as an Intergovernmental Organization
 ## or submit itself to any jurisdiction.
 
-#import fido
 from __future__ import print_function
-from invenio.base.globals import cfg
-from flask import current_app
-from invenio.modules.records.api import get_record
-
-# get_record(recid)
-
-# from invenio.legacy.bibdocfile.api import BibRecDocs
-#         return dict(
-#             zenodo_files=[f for f in BibRecDocs(
-#                 kwargs['recid'], human_readable=True
-#             ).list_latest_files(
-#                 list_hidden=False
-#             ) if not f.is_icon()]
-#         )
 
 
 def get_extension(file_path):
@@ -56,15 +41,28 @@ def get_name(file_path):
     return file_name
 
 
-#def calculate_score(file_path_list):
-def calculate_score(recid):
-    '''
+def calculate_score(file_path_list):
+    """
     TODO:
         * Pass files by argument
         * Use fido for file verification
-    '''
-    #cfg['PRESERVATIONMETER_dfdfd']
-    ## Map of quality per file type per category
+    """
+    ## Storing qualities
+    ## Iterate the list and get the file extension and quality associated.
+    files_quality = []
+    for file_p in file_path_list:
+        files_quality.append(calculate_file_score(file_p))
+
+    ## Average quality of this submission
+    return sum(files_quality) / len(files_quality)
+
+
+def calculate_file_score(file_name):
+    """Returns the associated score of this extension.
+
+    As defined on cfg.
+    """
+    file_ext = get_extension(file_name)
     ext_quality = {'.csv': 100,
                    '.pdf': 100,
                    '.txt': 95,
@@ -73,39 +71,4 @@ def calculate_score(recid):
                    '.docx': 60,
                    '.xls': 40,
                    '.doc': 40}
-
-    ## Get the files from this record
-    record = get_record(recid)
-    #for ifile in record['files_to_upload']:
-    #    print ifile['url']
-
-    ##TODO: Use this? v
-    #from invenio.legacy.bibdocfile.api import BibRecDocs
-    # files = dict(
-    #     zenodo_files=[f for f in BibRecDocs(
-    #         recid, human_readable=True
-    #     ).list_latest_files(
-    #         list_hidden=False
-    #     ) if not f.is_icon()]
-    # )
-
-    ## List of files to parse
-    ## Use the API to get the list.
-    #file_list = ['text.txt', 'data.csv']
-    #file_list = record['_files'] -> Doesnt work
-    file_list = record['files_to_upload']
-
-    ## Storing qualities
-    ## Iterate the list and get the file extension and quality associated.
-    files_quality = []
-    for ifile in file_list:
-        file_ext = get_extension(ifile['url'])
-        file_name = get_name(ifile['url'])
-        files_quality.append(ext_quality[file_ext])
-        current_app.logger.info('[{}{}] preservation status: {}'.format(
-                                file_name,
-                                file_ext,
-                                ext_quality[file_ext]))
-
-    ## Average quality of this submission
-    return sum(files_quality) / len(files_quality)
+    return ext_quality[file_ext]
