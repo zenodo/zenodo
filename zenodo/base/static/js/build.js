1d3667823d31dea7e5c491f40af9400ba17f9d51
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-10-30 08:56:31 +0100

    base: build.js addition
    * Fixes issue when using link collect storage. RequireJS build
      configuration from Invenio would pickup its own settings.js when
      using symlink storage.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

---
+++ zenodo/base/static/js/build.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2014 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Invenio is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Invenio; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+({
+    preserveLicenseComments: false,
+    optimize: 'uglify2',
+    uglify2: {
+        output: {
+            beautify: false,
+            comments: false
+        },
+        compress: {
+            drop_console: true,
+            sequences: true,
+            dead_code: true,
+            conditionals: true,
+            booleans: true,
+            unused: true,
+            if_return: true,
+            join_vars: true
+        },
+        warnings: true,
+        mangle: true
+    },
+    mainConfigFile: './settings.js',
+})
