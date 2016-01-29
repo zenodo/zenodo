0617079b85ba19706c34fbe8f00332925d34634e
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2016-01-29 15:40:13 +0100

    records: json and marcxml serialization
    * Adds initial JSON and MARCXML serialization for records.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

---
+++ zenodo/modules/records/serializers/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Zenodo.
+# Copyright (C) 2016 CERN.
+#
+# Zenodo is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Zenodo is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Zenodo; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Record serialization."""
+
+from __future__ import absolute_import, print_function
+
+from dojson.contrib.to_marc21 import to_marc21
+
+from .json import JSONSerializer
+from .marcxml import MARCXMLSerializer
+from .response import record_responsify, search_responsify
+from .schemas.json import RecordSchemaJSONV1
+from .schemas.marcxml import RecordSchemaMARC
+
+
+json_v1 = JSONSerializer(RecordSchemaJSONV1)
+marcxml_v1 = MARCXMLSerializer(to_marc21, schema_class=RecordSchemaMARC)
+# datacite_v1 = None
+# bibtex_v1 = None
+
+json_v1_response = record_responsify(json_v1, 'application/json')
+marcxml_v1_response = record_responsify(marcxml_v1, 'application/marc+xml')
+# datacite_v1_response = record_responsify(
+#    datacite_v1, 'application/x-datacite+xml')
+# bibtex_v1_response = record_responsify(bibtex_v1, 'application/x-bibtex')
+
+json_v1_search = search_responsify(json_v1, 'application/json')
+marcxml_v1_search = search_responsify(marcxml_v1, 'application/marc+xml')
+# datacite_v1_search = search_responsify(
+#     datacite_v1, 'application/x-datacite+xml')
+# bibtex_v1_search = search_responsify(bibtex_v1, 'application/x-bibtex')
--- zenodo/modules/theme/static/js/base.js
+++
@@ -1,7 +0,0 @@
-require([
-    "jquery",
-    "bootstrap",
-    ], function() {
-    // loading all the jQuery modules for the not require.js ready scripts
-    // everywhere.
-})
