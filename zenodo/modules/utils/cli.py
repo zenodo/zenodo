b01f0db59a819e5d0707a63ca5771243b4f35905
Krzysztof Nowak <k.nowak@cern.ch>
2016-09-27 16:59:40 +0200

    cli: addition of file curation CLI
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- zenodo/modules/utils/cli.py
+++ zenodo/modules/utils/cli.py
@@ -26,17 +26,22 @@
 
 from __future__ import absolute_import, print_function
 
+import os
+from io import SEEK_END, SEEK_SET
+
 import click
 from flask_cli import with_appcontext
-from invenio_pidstore.resolver import Resolver
-from invenio_records.api import Record
+from invenio_db import db
+from invenio_files_rest.models import ObjectVersion
 
 from zenodo.modules.deposit.tasks import datacite_register
+from zenodo.modules.records.api import record_resolver
 
 
 @click.group()
 def utils():
     """Zenodo helper CLI."""
+
 
 @utils.command('datacite_register')
 @click.argument('recid', type=str)
@@ -44,10 +49,179 @@ def utils():
 @with_appcontext
 def datecite_register(recid, eager):
     """Send a record to DataCite for registration."""
-    pid, record = Resolver(
-        pid_type='recid', object_type='rec',
-        getter=Record.get_record).resolve(recid)
+    pid, record = record_resolver.resolve(recid)
     if eager:
         datacite_register.s(pid.pid_value, str(record.id)).apply(throw=True)
     else:
         datacite_register.s(pid.pid_value, str(record.id)).apply_async()
+
+
+@utils.command('add_file')
+@click.argument('recid', type=str)
+@click.argument('fp', type=click.File('rb'))
+@click.option('--replace-existing', '-f', is_flag=True, default=False)
+@with_appcontext
+def add_file(recid, fp, replace_existing):
+    """Add a new file to a publishd record."""
+    pid, record = record_resolver.resolve(recid)
+    bucket = record.files.bucket
+    key = os.path.basename(fp.name)
+
+    obj = ObjectVersion.get(bucket, key)
+    if obj is not None and not replace_existing:
+        click.echo(click.style('File with key "{key}" alreay exists.'
+                   ' Use `--replace-existing/-f` to overwrite it.'.format(
+                        key=key, recid=recid), fg='red'))
+        return
+
+    fp.seek(SEEK_SET, SEEK_END)
+    size = fp.tell()
+    fp.seek(SEEK_SET)
+
+    click.echo('Will add the following file:\n')
+    click.echo(click.style(
+        '  key: "{key}"\n'
+        '  bucket: {bucket}\n'
+        '  size: {size}\n'
+        ''.format(
+            key=key,
+            bucket=bucket.id,
+            size=size),
+        fg='orange'))
+    click.echo('to record:\n')
+    click.echo(click.style(
+        '  Title: "{title}"\n'
+        '  RECID: {recid}\n'
+        '  UUID: {uuid}\n'
+        ''.format(
+            recid=record['recid'],
+            title=record['title'],
+            uuid=record.id),
+        fg='green'))
+    if replace_existing and obj is not None:
+        click.echo('and remove the file:\n')
+        click.echo(click.style(
+            '  key: "{key}"\n'
+            '  bucket: {bucket}\n'
+            '  size: {size}\n'
+            ''.format(
+                key=obj.key,
+                bucket=obj.bucket,
+                size=obj.file.size),
+            fg='green'))
+
+    if click.confirm('Continue?'):
+        bucket.locked = False
+        if obj is not None and replace_existing:
+            ObjectVersion.delete(bucket, obj.key)
+        ObjectVersion.create(bucket, key, stream=fp)
+        bucket.locked = True
+
+        record.files.flush()
+        record.commit()
+        db.session.commit()
+        click.echo(click.style('File added successfully.', fg='green'))
+    else:
+        click.echo(click.style('File addition aborted.', fg='green'))
+
+
+@utils.command('remove_file')
+@click.argument('recid', type=str)
+@click.argument('key', type=str)
+@with_appcontext
+def remove_file(recid, key=None, index=None):
+    """Remove a file from a publishd record."""
+    pid, record = record_resolver.resolve(recid)
+    bucket = record.files.bucket
+    obj = ObjectVersion.get(bucket, key)
+    if obj is None:
+        click.echo(click.style('File with key "{key}" not found.'.format(
+            key=key, recid=recid), fg='red'))
+        return
+
+    click.echo('Will remove the following file:\n')
+    click.echo(click.style(
+        '  key: "{key}"\n'
+        '  {checksum}\n'
+        '  bucket: {bucket}\n'
+        ''.format(
+            key=key,
+            checksum=obj.file.checksum,
+            bucket=bucket.id),
+        fg='green'))
+    click.echo('from record:\n')
+    click.echo(click.style(
+        '  Title: "{title}"\n'
+        '  RECID: {recid}\n'
+        '  UUID: {uuid}\n'
+        ''.format(
+            recid=record['recid'],
+            title=record['title'],
+            uuid=record.id),
+        fg='green'))
+
+    if click.confirm('Continue?'):
+        bucket.locked = False
+        ObjectVersion.delete(bucket, obj.key)
+        bucket.locked = True
+        record.files.flush()
+        record.commit()
+        db.session.commit()
+        click.echo(click.style('File removed successfully.', fg='green'))
+    else:
+        click.echo(click.style('Aborted file removal.', fg='green'))
+
+
+@utils.command('rename_file')
+@click.argument('recid', type=str)
+@click.argument('key', type=str)
+@click.argument('new_key', type=str)
+@with_appcontext
+def rename_file(recid, key, new_key):
+    """Remove a file from a publishd record."""
+    pid, record = record_resolver.resolve(recid)
+    bucket = record.files.bucket
+
+    obj = ObjectVersion.get(bucket, key)
+    if obj is None:
+        click.echo(click.style('File with key "{key}" not found.'.format(
+            key=key), fg='red'))
+        return
+
+    new_obj = ObjectVersion.get(bucket, new_key)
+    if new_obj is not None:
+        click.echo(click.style('File with key "{key}" already exists.'.format(
+            key=new_key), fg='red'))
+        return
+
+    if click.confirm('Rename "{key}" to "{new_key}" on bucket {bucket}.'
+                     ' Continue?'.format(
+                        key=obj.key, new_key=new_key, bucket=bucket.id)):
+        record.files.bucket.locked = False
+
+        file_id = obj.file.id
+        ObjectVersion.delete(bucket, obj.key)
+        ObjectVersion.create(bucket, new_key, _file_id=file_id)
+        record.files.bucket.locked = True
+        record.files.flush()
+        record.commit()
+        db.session.commit()
+        click.echo(click.style('File renamed successfully.', fg='green'))
+    else:
+        click.echo(click.style('Aborted file rename.', fg='green'))
+
+
+@utils.command('list_files')
+@click.argument('recid', type=str)
+@with_appcontext
+def list_files(recid):
+    """List files for the record."""
+    pid, record = record_resolver.resolve(recid)
+    click.echo('Files for record {recid} (UUID:{uuid}) ({cnt} file(s)):\n'
+               ''.format(recid=recid, uuid=record.id, cnt=len(record.files)))
+    for idx, key in enumerate(record.files.keys):
+        f = record.files[key].obj.file
+        click.echo(click.style(
+            '{idx:3}: "{key}", {checksum}, size:{size}'
+            ''.format(idx=idx, key=key, checksum=f.checksum, size=f.size),
+            fg='green'))
