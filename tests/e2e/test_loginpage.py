31426bab6cde4583e6b8d326e41d5a8b8451c7f3
Rajat Arora <rajat1994@users.noreply.github.com>
2017-03-07 20:55:55 +0530

    test_loginpage.py

---
+++ tests/e2e/test_loginpage.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def test_loginpage(live_server, env_browser):
+ user = ""
+ pwd = ""
+ env_browser.get("https://zenodo.org/")
+ elem = env_browser.find_element_by_id("email")
+ elem.send_keys(user)
+ elem = env_browser.find_element_by_id("pass")
+ elem.send_keys(pwd)
+ elem.send_keys(Keys.RETURN)
+ env_browser.close()
