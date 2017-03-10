e3be2c2b89566f9d78aaa13341e8cb51ac5c6c8f
Rajat Arora <rajat1994@users.noreply.github.com>
2017-03-10 20:22:59 +0530

    updated test_schemas
    i have validated the full_record fixture similar to minimal.

--- tests/unit/records/test_schemas.py
+++ tests/unit/records/test_schemas.py
@@ -46,7 +46,7 @@ def test_recid(app, minimal_record):
 
 
 def test_resource_type(app, db, minimal_record):
-    """Test recid property."""
+    """Test resource type."""
     # String instead of number
     minimal_record['resource_type'] = 'publication'
     pytest.raises(ValidationError, Record.create, minimal_record)
@@ -55,14 +55,14 @@ def test_resource_type(app, db, minimal_record):
 
 
 def test_publication_date(app, db, minimal_record):
-    """Test recid property."""
+    """Test publication date."""
     # String instead of number
     minimal_record['publication_date'] = datetime.utcnow().date().isoformat()
     Record.create(minimal_record)
 
-
+    
 def test_contributors(app, db, minimal_record):
-    """Test recid property."""
+    """Test contributors."""
     # String instead of number
     minimal_record['contributors'] = [
         {'name': 'test', 'affiliation': 'test', 'type': 'ContactPerson'}
@@ -72,3 +72,42 @@ def test_contributors(app, db, minimal_record):
         {'name': 'test', 'affiliation': 'test', 'type': 'Invalid'}
     ]
     pytest.raises(ValidationError, Record.create, minimal_record)
+     # validation of full_record fixture
+    
+def test_full_json(app, db, full_record) :
+      """Test full json."""
+    Record.create( full_record)
+      
+def full_test_recid(app,db,full_record):
+    """Test recid property."""
+    
+    full_record['recid'] = '12345'
+    pytest.raises(ValidationError, Record.create, full_record)
+      
+def full_test_resource_type(app, db, full_record):
+    """Test resource type."""
+    
+    full_record['resource_type'] = 'publication'
+    pytest.raises(ValidationError, Record.create, full_record)
+    full_record['resource_type'] = {'type': 'publication', 'subtype': 'book'}
+    Record.create(full_record) 
+    
+def full_test_contributors(app, db, full_record):
+    """Test contributors."""
+    
+    full_record['contributors'] = [
+       {'affiliation': 'CERN', 'name': 'Smith, Other', 'type': 'Other',
+             'gnd': '', 'orcid': '0000-0002-1825-0097'}
+    ]
+    Record.create(full_record)
+    full_record['contributors'] = [
+        {'affiliation': '', 'name': 'Hansen, Viggo', 'type': 'Other',
+             'gnd': '', 'orcid': ''} 
+  ]
+    pytest.raises(ValidationError, Record.create, full_record)     
+    
+    
+    
+      
+     
+
