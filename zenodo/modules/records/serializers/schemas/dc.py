d5c4ba89c7a8641f205629bf0e6c778c85ff5244
Rachit Dhamija <rachit.dhamija.96@gmail.com>
2017-03-17 03:00:51 +0530

    records: Dublin Core HTML strip from description fix #792
    Strips all html tags from dc:description, so that the description is displayed without html after serailization.

--- zenodo/modules/records/serializers/schemas/dc.py
+++ zenodo/modules/records/serializers/schemas/dc.py
@@ -30,6 +30,25 @@ from marshmallow import Schema, fields
 
 from ...models import ObjectType
 
+from html.parser import HTMLParser
+
+
+class HTMLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = HTMLStripper()
+    s.feed(html)
+    return s.get_data()
+
 
 class DublinCoreV1(Schema):
     """Schema for records v1 in JSON."""
@@ -109,7 +128,7 @@ class DublinCoreV1(Schema):
 
     def get_descriptions(self, obj):
         """Get descriptions."""
-        return [obj['metadata']['description']]
+        return [strip_tags(obj['metadata']['description'])]
 
     def get_subjects(self, obj):
         """Get subjects."""
