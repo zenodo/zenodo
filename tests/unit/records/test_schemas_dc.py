124228ad8358d6d67a93331f424f848bb0266e81
rd93 <rachit.dhamija.96@gmail.com>
2017-03-18 14:43:25 +0530

    updated description test

--- tests/unit/records/test_schemas_dc.py
+++ tests/unit/records/test_schemas_dc.py
@@ -188,14 +188,12 @@ def test_sources_meetings(minimal_record, recid_pid):
 
 def test_description(minimal_record, recid_pid):
     """Test description"""
-    minimal_record.update({
-        'descriptions': ['<p>TRPC1 and store-operated Ca2+&nbsp;(SOC) entry \
+    minimal_record['descriptions']: ['<p>TRPC1 and store-operated Ca2+&nbsp;(SOC) entry \
     have previously been associated. The aim of the study was to determine \
     genes and processes associated with&nbsp;<em>TRPC1 </em>&nbsp;down- \
     regulation and the resulting increase of SOC entry. A total of 71 \
     genes were significantly differentially expressed (40 up- and 31 down- \
     regulated).</p>']
-    })
     obj = dc_v1.transform_record(recid_pid, Record(minimal_record))
     assert obj['descriptions'] == ['TRPC1 and store-operated Ca2+ (SOC) \
     entry have previously been associated. The aim of the study was to \
