8fef34366271b54a4156ff23095efd75529cb096
Vedant Rathore <vedantr1998@gmail.com>
2017-03-28 03:17:37 +0530

    Fix Issue #997
    Added a clean_input method also a test case to validate the same.

--- zenodo/modules/records/serializers/bibtex.py
+++ zenodo/modules/records/serializers/bibtex.py
@@ -145,10 +145,10 @@ class Bibtex(object):
 
     def _clean_input(self,input):
         unsupported_char = ['&']
-        chars = input.split('')
-        for char in chars:
+        chars = list(input)
+        for index, char in enumerate(chars):
             if char in unsupported_char:
-                char = "\\" +char
+                chars[index] = "\\" + chars[index]
         return ''.join(chars)
 
     def _format_article(self):
