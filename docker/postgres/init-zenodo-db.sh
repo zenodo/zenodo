dace09b1b0eb33680071666fbd1ec4f860db7177
Alexander Ioannidis <a.ioannidis@cern.ch>
2017-05-26 17:34:50 +0200

    docker: set zenodo user as db owner
    Signed-off-by: Alexander Ioannidis <a.ioannidis@cern.ch>

--- docker/postgres/init-zenodo-db.sh
+++ docker/postgres/init-zenodo-db.sh
@@ -3,6 +3,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER zenodo WITH CREATEDB PASSWORD 'zenodo';
-    CREATE DATABASE zenodo;
+    CREATE DATABASE zenodo OWNER zenodo;
     GRANT ALL PRIVILEGES ON DATABASE zenodo TO zenodo;
 EOSQL
