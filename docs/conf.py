128dffe27656885b769d41487992959e67b8f676
Aman Jain <jainaman224@gmail.com>
2017-06-29 18:44:31 +0530

    docs: sphinx fix
    Signed-off-by: Aman Jain <jainaman224@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -341,4 +341,6 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None)
+}
