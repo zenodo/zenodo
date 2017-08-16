1739f5e27992c1013d7730c7bd9a7c7b86acb674
Alexander Ioannidis <a.ioannidis@cern.ch>
2017-08-16 15:00:41 +0200

    scripts: invenio-access command syntax fix
    Signed-off-by: Alexander Ioannidis <a.ioannidis@cern.ch>

--- scripts/recreate.sh
+++ scripts/recreate.sh
@@ -39,6 +39,6 @@ zenodo fixtures loadfunders
 zenodo fixtures loadfp6grants
 zenodo fixtures loadsipmetadatatypes
 zenodo users create info@zenodo.org -a --password=123456
-zenodo access allow admin-access -e info@zenodo.org
-zenodo access allow deposit-admin-access -e info@zenodo.org
+zenodo access allow admin-access user info@zenodo.org
+zenodo access allow deposit-admin-access user info@zenodo.org
 zenodo fixtures loadcommunities info@zenodo.org
