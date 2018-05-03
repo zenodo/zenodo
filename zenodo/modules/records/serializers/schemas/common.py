989ca8d29b1cea17919f96b134b03106d49ebec3
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2018-05-03 16:21:00 +0200

    records: file key encoding problem

--- zenodo/modules/records/serializers/schemas/common.py
+++ zenodo/modules/records/serializers/schemas/common.py
@@ -80,7 +80,7 @@ def link_for(base, tpl, **kwargs):
     tpl = URLS.get(tpl)
     for k in ['key', ]:
         if k in kwargs:
-            kwargs[k] = quote(kwargs[k])
+            kwargs[k] = quote(kwargs[k].encode('utf8'))
     return tpl.format(base=base, **kwargs)
 
 
