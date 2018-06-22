554ad02f9e269e1cfaf1480369c2f4881f417839
Chiara Bigarella <chiara.bigarella1@gmail.com>
2018-06-22 14:41:33 +0200

    records: emit record_viewed event during export

--- zenodo/modules/records/views.py
+++ zenodo/modules/records/views.py
@@ -39,6 +39,7 @@ from invenio_formatter.filters.datetime import from_isodate
 from invenio_i18n.ext import current_i18n
 from invenio_pidstore.models import PersistentIdentifier, PIDStatus
 from invenio_previewer.proxies import current_previewer
+from invenio_records_ui.signals import record_viewed
 from werkzeug.utils import import_string
 
 from zenodo.modules.communities.api import ZenodoCommunity
@@ -332,6 +333,12 @@ def records_ui_export(pid, record, template=None, **kwargs):
         if isinstance(data, six.binary_type):
             data = data.decode('utf8')
 
+        # emit record_viewed event
+        record_viewed.send(
+            current_app._get_current_object(),
+            pid=pid,
+            record=record,
+        )
         return render_template(
             template, pid=pid, record=record,
             data=data, format_title=formats[fmt]['title'])
