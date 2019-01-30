9bf90a2c97c02a7c9978b36169a3a3a1c0797d29
Chiara Bigarella <chiara.bigarella1@gmail.com>
2019-01-30 11:02:19 +0100

    openaire: add tests

--- tests/unit/openaire/test_openaire_helpers.py
+++ tests/unit/openaire/test_openaire_helpers.py
@@ -69,8 +69,12 @@ def test_openaire_id(app, minimal_record):
     r['doi'] = u'10.5281/zenodo.123'
     r['_oai'] = {'id': u'oai:zenodo.org:123'}
 
-    # Default zenodo type is software which has no OpenAIRE type.
-    assert openaire_id(r) is None
+    # Default zenodo type is software
+    assert openaire_id(r) == 'r37b0ad08687::47287d1800c112499a117ca17aa1909d'
+
+    # Other type
+    r['resource_type']['type'] = 'other'
+    assert openaire_id(r) == 'r37b0ad08687::47287d1800c112499a117ca17aa1909d'
 
     # Dataset ID
     r['resource_type']['type'] = 'dataset'
@@ -87,8 +91,16 @@ def test_openaire_link(app, minimal_record):
     r['doi'] = u'10.5281/zenodo.123'
     r['_oai'] = {'id': u'oai:zenodo.org:123'}
 
-    # Default zenodo type is software which has no OpenAIRE type.
-    assert openaire_link(r) is None
+    # Default zenodo type is software
+    assert openaire_link(r) ==\
+        'https://beta.openaire.eu/search/software?' \
+        'softwareId=r37b0ad08687::47287d1800c112499a117ca17aa1909d'
+
+    # Other type
+    r['resource_type']['type'] = 'other'
+    assert openaire_link(r) == \
+        'https://beta.openaire.eu/search/other?' \
+        'orpId=r37b0ad08687::47287d1800c112499a117ca17aa1909d'
 
     # Dataset ID
     r['resource_type']['type'] = 'dataset'
