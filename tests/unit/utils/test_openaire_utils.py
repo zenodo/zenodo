8397ef5558efa9eda1ebd7d281460ca5bd72907e
Chiara Bigarella <chiara.bigarella1@gmail.com>
2019-02-26 17:53:22 +0100

    utils: add sync_community_mappings command

--- tests/unit/utils/test_openaire_utils.py
+++ tests/unit/utils/test_openaire_utils.py
@@ -24,26 +24,26 @@
 
 """Test OpenAIRE community mapping."""
 
-from zenodo.modules.utils.openaire import create_communities,\
-    get_new_communites
+from zenodo.modules.utils.openaire import create_communities, \
+    compare_community_mappings, get_new_communites, update_communities_mapping
 
 
-def get_communities_mapping(user_emails):
+def _new_communities_mapping(user_emails):
     return {
         'oa_comm1': dict(name='Community num 1',
-                         communities=[],
+                         communities=['zenodo'],
                          types=[],
                          primary_community='zenodo',
                          curators=[user_emails[0]]),
         'oa_comm2': dict(name='Community num 2',
-                         communities=[],
+                         communities=['ecfunded', 'grants_comm', 'wrong_comm'],
                          types=[],
                          primary_community='z_comm2',
                          curators=[user_emails[1]]),
     }
 
 
-def get_new_communities(user_id):
+def _new_communities(user_id):
     return [
         dict(id='z_comm2',
              title='Community num 2',
@@ -54,22 +54,75 @@ def get_new_communities(user_id):
 def test_get_new_communites(db, users, communities):
     """Test get new communities."""
     user_emails = [users[0]['email'], users[1]['email']]
-    communities = get_communities_mapping(user_emails)
-    new_communities = get_new_communities(users[1]['id'])
+    new_mapping = _new_communities_mapping(user_emails)
+    new_communities = _new_communities(users[1]['id'])
 
-    result = get_new_communites(communities)
+    result = get_new_communites(new_mapping)
 
     assert result == new_communities
 
 
 def test_create_communities(db, users, communities):
-    """Test create community."""
+    """Test create a new community."""
     user_emails = [users[0]['email'], users[1]['email']]
-    communities = get_communities_mapping(user_emails)
-    new_communities = get_new_communities(users[1]['id'])
+    new_mapping = _new_communities_mapping(user_emails)
+
+    new_communities = get_new_communites(new_mapping)
+    assert new_communities != []
 
     create_communities(new_communities)
 
-    result = get_new_communites(communities)
+    result = get_new_communites(new_mapping)
 
     assert result == []
+
+
+def test_compare_community_mappings(db, users):
+    """Test compare community mappings."""
+    user_emails = [users[0]['email'], users[1]['email']]
+    new_mapping = _new_communities_mapping(user_emails)['oa_comm2']
+    current_mapping = dict(name='Community num 2',
+                           communities=['ecfunded'],
+                           types=[],)
+    result = compare_community_mappings(current_mapping, new_mapping)
+    assert result == {'communities': ['ecfunded', 'grants_comm', 'wrong_comm'],
+                      'primary_community': 'z_comm2'}
+
+
+def test_update_communities_mapping(db, users, communities):
+    """Test update communities mapping."""
+    user_emails = [users[0]['email'], users[1]['email']]
+    new_mapping = _new_communities_mapping(user_emails)
+    current_mapping = {
+        'oa_comm1': dict(name='Community num 1',
+                         communities=['zenodo'],
+                         types=[],
+                         primary_community='zenodo',
+                         curators=[user_emails[0]]),
+        'oa_comm2': dict(name='Community num 2',
+                         communities=['ecfunded'],
+                         types=[],
+                         primary_community='z_comm2',
+                         curators=[user_emails[1]]),
+
+
+    }
+
+    updated_mapping, diff, unresolved_comm = \
+        update_communities_mapping(current_mapping, new_mapping)
+
+    assert updated_mapping == {
+        'oa_comm1': dict(name='Community num 1',
+                         communities=['zenodo'],
+                         primary_community='zenodo',
+                         types=[],),
+        'oa_comm2': dict(name='Community num 2',
+                         communities=['ecfunded', 'grants_comm'],
+                         primary_community='z_comm2',
+                         types=[],),
+
+
+    }
+    assert diff == {'oa_comm2': {'communities': ['ecfunded', 'grants_comm']}}
+    assert unresolved_comm == [dict(openaire_community='oa_comm2',
+                                    zenodo_community='wrong_comm')]
--- zenodo/modules/utils/openaire.py
+++ zenodo/modules/utils/openaire.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Zenodo.
-# Copyright (C) 2018 CERN.
+# Copyright (C) 2018-2019 CERN.
 #
 # Zenodo is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -26,6 +26,7 @@
 
 import csv
 import re
+from collections import OrderedDict
 import requests
 
 from flask import current_app
@@ -180,3 +181,76 @@ def create_communities(new_communities):
                              user_id=comm['owner'],
                              title=comm['title'])
             db.session.commit()
+
+
+def compare_community_mappings(current, new):
+    """Compare the current and the new mappings of a community."""
+    # 1) check if the title has changed
+    # 2) check if the primary community has changed
+    # 3) check if the communities have changed
+
+    diff = {}
+    if current['name'] != new['name']:
+        diff['name'] = new['name']
+
+    current_primary_community = current.get('primary_community')
+    if current_primary_community != new['primary_community']:
+        if current_primary_community is not None:
+            raise Exception("Primary community cannot change.")
+        diff['primary_community'] = new['primary_community']
+
+    if current['communities'] != new['communities']:
+        if len(new['communities']) < len(current['communities']):
+            raise Exception("Communities cannot be deleted.")
+        for comm in current['communities']:
+            if comm not in new['communities']:
+                raise Exception("Communities cannot be deleted.")
+        diff['communities'] = new['communities']
+
+    return diff
+
+
+def update_communities_mapping(current_mapping, new_mapping):
+    """Update communities mapping."""
+    unresolved_communities = []
+    updated_mapping = {}
+    diff = {}
+
+    for oa_community_id in new_mapping:
+        openaire_community = new_mapping[oa_community_id]
+
+        # here we don't check if the primary_community is a real Zenodo
+        # community; this is checked in the get_new_communites() method
+
+        zenodo_community_ids = []
+        for zenodo_community in openaire_community.get('communities'):
+            if _community_exists(zenodo_community):
+                zenodo_community_ids.append(zenodo_community)
+            else:
+                unresolved_communities.append(dict(
+                    openaire_community=oa_community_id,
+                    zenodo_community=zenodo_community))
+        openaire_community['communities'] = zenodo_community_ids
+
+        community_types = None  # community types are not provided by the APIs
+        current = current_mapping.get(oa_community_id)
+        if current:
+            community_types = current['types']
+
+            # compare current and new mappings
+            mapping_diff = \
+                compare_community_mappings(current, openaire_community)
+            if mapping_diff:
+                diff[oa_community_id] = mapping_diff
+
+        updated_mapping[oa_community_id] = dict(
+            name=openaire_community['name'],
+            communities=zenodo_community_ids,
+            primary_community=openaire_community['primary_community'],
+            types=community_types,
+        )
+
+    # the updated_mapping should be in alphabetical order
+    updated_mapping = OrderedDict(sorted(updated_mapping.items()))
+
+    return updated_mapping, diff, unresolved_communities
