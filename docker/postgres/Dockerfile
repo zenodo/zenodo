7d771d857425e5f0a1b8e521294a48c833daab5b
Alexander Ioannidis <a.ioannidis@cern.ch>
2019-09-04 15:19:03 +0200

    docker: simplify "db" container

--- docker/postgres/Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM postgres:9.6.2
-COPY ./init-zenodo-db.sh /docker-entrypoint-initdb.d/10-init-zenodo-db.sh
--- docker/postgres/init-zenodo-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER zenodo WITH CREATEDB PASSWORD 'zenodo';
-    CREATE DATABASE zenodo OWNER zenodo;
-    GRANT ALL PRIVILEGES ON DATABASE zenodo TO zenodo;
-EOSQL
